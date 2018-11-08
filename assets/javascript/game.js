567e7ff4409decbf835d32f49841932d93366469
Narwhal Nancy <dylanawhite11@gmail.com>
2018-11-08 12:23:26 -0600

    js variables added

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var alphabet =[
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", 
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+    "s", "t", "u", "v", "w", "x", "y", "z"
+];
+
+var wins = 0;
+
+var losses = 0;
+
+var guessesMade = 0;
+
+var guessesLeft = 10;
